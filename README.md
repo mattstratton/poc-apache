3537c527c0b4f0be84880f3c014bb881dbe61d12
Chef Software <chef@chef.io>
2016-01-26 16:45:08 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# securian-apache
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'securian-apache'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures securian-apache'
+long_description 'Installs/Configures securian-apache'
+version '0.1.0'
