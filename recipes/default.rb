c8f08b3b516f3aab13bd3cf8ee904adfd0f6f54c
Chef Software <chef@chef.io>
2016-01-26 21:29:38 +0000

    change to cent 6 and apached 2.2 and disable selinux

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,17 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-package 'httpd'
+execute 'setenforce 0'
+
+package 'httpd' do
+   version '2.2.15-45.el6.centos'
+  action :install
+end
 
 user node['securian-apache']['apache-user'] do
   comment 'Apache'
   system true
-  shell '/bin/false'
+  shell '/sbin/nologin'
 end
 
 directory node['securian-apache']['webroot'] do
@@ -26,10 +31,25 @@ directory node['securian-apache']['logroot'] do
   recursive true
 end
 
+directory "#{node['securian-apache']['webroot']}/default/production/root" do
+  owner node['securian-apache']['apache-user']
+  mode '0755'
+  group node['securian-apache']['apache-group']
+  recursive true
+end
+directory "#{node['securian-apache']['logroot']}/websites/default" do
+  owner node['securian-apache']['apache-user']
+  mode '0755'
+  group node['securian-apache']['apache-group']
+  recursive true
+end
+
 template '/etc/httpd/conf/httpd.conf' do
   source 'httpd.conf.erb'
+  notifies :reload, 'service[httpd]'
 end
 
 service 'httpd' do
   action [:enable, :start]
+  supports :restart => true, :reload => true
 end
