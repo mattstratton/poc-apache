fc52a882d8fbddd078da58f90d1e8a0bf37eb07c
Chef Software <chef@chef.io>
2016-01-27 10:49:48 -0600

    adding vhost features

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default['securian-apache']['webroot'] = '/data/websites'
 default['securian-apache']['logroot'] = '/data/logs'
 default['securian-apache']['apache-user'] = 'apache'
 default['securian-apache']['apache-group'] = 'apache'
+default['securian-apache']['sites']['mysite'] = { 'port' => 80 }
+default['securian-apache']['sites']['othersite'] = { 'port' => 81 }
